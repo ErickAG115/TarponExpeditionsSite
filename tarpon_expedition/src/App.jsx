520347b8f69aba5f17fbae2d4f9f47e2c2063b5e
Erick <ag.erick115@gmail.com>
2023-04-14 22:32:03 -0600

    Design Sign In

--- tarpon_expedition/src/App.jsx
+++ tarpon_expedition/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Fragment, useRef, useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { v4 as uuid } from "uuid";
 import { Login } from "./components/website/Login";
+import { Register } from "./components/website/Register";
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
       <div className="App">
         <Routes>
           <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
         </Routes>
       </div>
     </Router>
--- tarpon_expedition/src/components/website/Login.jsx
+++ tarpon_expedition/src/components/website/Login.jsx
@@ -5,7 +5,28 @@ import { useNavigate } from "react-router-dom";
 export function Login() {
     return (
         <Fragment>
-            hola
+            <div style={{backgroundColor: '#D2D7DB', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh',}}>
+                <div style={{float: 'left', width: '50%', height:'100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <img src={require('./TarponLogo.png')} alt={'tarpon logo'} style={{height:'50%',width:'50%'}}/>
+                </div>
+                <div style={{float: 'right', width: '50%', height:'100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'auto'}}>
+                    <div style={{backgroundColor: 'white', height:'80%', width:'80%', display: 'flex', flexDirection: 'column', overflow: 'auto', alignItems: 'center', justifyContent: 'center'}}>
+                        <label style={{fontFamily: 'Rosario', fontSize: '30px', fontWeight:'bold', marginBottom:'40px'}}>Sign In</label>
+
+                        <label style={{fontFamily: 'Rosario', fontSize: '20px', position: 'relative', left: '-55px'}}>Email Address</label>
+                        <input type="text" id="input1" style={{ borderRadius: '5px', width:'45%', marginBottom:'20px'}} />
+
+                        <label style={{fontFamily: 'Rosario', fontSize: '20px', position: 'relative', left: '-75px'}}>Password</label>
+                        <input type="text" id="input2" style={{ borderRadius: '5px', width:'45%'}} />
+                        <button style={{color: 'blue', fontFamily: 'Rosario', fontSize: '15px', position: 'relative', left: '-60px', marginBottom:'20px', border: 'none'}}>Forgot Password?</button>
+
+                    <div style={{height:'20%', width:'100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'auto'}}>
+                        <button style={{width:'30%', fontSize: '20px', fontFamily: 'Rosario', backgroundColor:'#24AFC1',color: 'white', border: 'none', borderRadius: '10px', marginRight:'10px'}}>Sign In</button>
+                        <button style={{width:'30%', fontSize: '20px', fontFamily: 'Rosario', backgroundColor:'#24AFC1',color: 'white', border: 'none', borderRadius: '10px', marginLeft:'10px'}}>Sign Up</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </Fragment>
     )
 }
