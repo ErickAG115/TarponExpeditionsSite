0d55d5e4b686f27557abcdefeb1c1074a0f99ec6
Erick <ag.erick115@gmail.com>
2023-04-26 12:07:26 -0600

    design fixes

--- tarpon_expedition/src/components/website/AboutUs.jsx
+++ tarpon_expedition/src/components/website/AboutUs.jsx
@@ -49,21 +49,21 @@ export function AboutUs() {
 
 
                     <div style={{width: '50%', height:'40%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                        <div style={{float: 'left', width: '50%', height:'120%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'auto', marginRight:'10px'}}>
+                        <div style={{float: 'left', width: '50%', height:'120%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'auto', marginRight:'10px', position: 'relative', top:'-20px'}}>
                             <img src={require('./Company Logo.jpg')} alt={'company logo'} style={{height:'60%',width:'100%', borderRadius: '10px'}}/>
                         </div>
-                        <div style={{float: 'right', width: '50%', height:'100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'auto', marginLeft:'10px', marginTop:'15px'}}>
+                        <div style={{float: 'right', width: '50%', height:'100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'auto', marginLeft:'10px', marginTop:'15px', position: 'relative', top:'-20px'}}>
                             <p style={{fontFamily:'lato'}}>In Tarpon Expedition, we believe in sustainable fishing practices that preserve the delicate ecosystem of the Caribbean coast. Our skilled guides will take you out to the best fishing spots in the area, where you can catch a variety of fish, including snapper, marlin, and tuna. We provide all the necessary equipment, including rods, reels, bait, and tackle.</p>
                         </div>
                     </div>
-                    <label style={{fontFamily: 'lato', fontSize: '30px', fontWeight:'bold', position: 'relative', left: '-290px'}}>Team</label>
-                    <div style={{width: '50%', height:'20%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                        <img src={require('./Team Member 1.jpg')} alt={'company logo'} style={{height:'110%',width:'80%', borderRadius: '10px', marginRight:'10px'}}/>
-                        <img src={require('./Team Member 2.jpg')} alt={'company logo'} style={{height:'110%',width:'80%', borderRadius: '10px'}}/>
-                        <img src={require('./Team Member 3.jpg')} alt={'company logo'} style={{height:'110%',width:'80%', borderRadius: '10px', marginLeft:'10px'}}/>
+                    <label style={{fontFamily: 'lato', fontSize: '30px', fontWeight:'bold', position: 'relative', left: '-370px', top:'-30px'}}>Team</label>
+                    <div style={{width: '50%', height:'20%', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative', top:'-30px'}}>
+                        <img src={require('./Team Member 1.jpg')} alt={'company logo'} style={{height:'110%',width:'90%', borderRadius: '10px', marginRight:'10px'}}/>
+                        <img src={require('./Team Member 2.jpg')} alt={'company logo'} style={{height:'110%',width:'90%', borderRadius: '10px'}}/>
+                        <img src={require('./Team Member 3.jpg')} alt={'company logo'} style={{height:'110%',width:'90%', borderRadius: '10px', marginLeft:'10px'}}/>
                     </div>
-                    <label style={{fontFamily: 'lato', fontSize: '30px', fontWeight:'bold', position: 'relative', left: '-250px'}}>Equipment</label>
-                    <div style={{width: '50%', height:'20%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <label style={{fontFamily: 'lato', fontSize: '30px', fontWeight:'bold', position: 'relative', left: '-380px', top:'-20px'}}>Equipment</label>
+                    <div style={{width: '60%', height:'20%', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative', top:'-20px'}}>
                         <img src={require('./Boats.jpg')} alt={'company logo'} style={{height:'100%',width:'80%', borderRadius: '10px', marginRight:'10px'}}/>
                         <img src={require('./Fishing Lures.jpg')} alt={'company logo'} style={{height:'100%',width:'80%', borderRadius: '10px'}}/>
                         <img src={require('./Fishing Rods.jpg')} alt={'company logo'} style={{height:'100%',width:'80%', borderRadius: '10px', marginLeft:'10px'}}/>
--- tarpon_expedition/src/components/website/Contact.jsx
+++ tarpon_expedition/src/components/website/Contact.jsx
@@ -47,16 +47,16 @@ export function Contact() {
             <label style={{fontFamily: 'Lato', fontSize: '40px', fontWeight:'bold', marginBottom:'20px'}}>Contact Information</label>
             <div style={{width: '50%', height:'40%', display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom:'30px'}}>
                 <div style={{float: 'left', width: '30%', height:'100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'auto',flexDirection: 'column', position: 'relative', left: '80px'}}>
-                    <img src={require('./Phone Number.png')} alt={'company logo'} style={{height:'35%',width:'20%', borderRadius: '10px'}}/>
-                    <img src={require('./Email.png')} alt={'company logo'} style={{height:'35%',width:'20%', borderRadius: '10px'}}/>
+                    <img src={require('./Phone Number.png')} alt={'company logo'} style={{height:'35%',width:'22%', borderRadius: '10px'}}/>
+                    <img src={require('./Email.png')} alt={'company logo'} style={{height:'35%',width:'22%', borderRadius: '10px'}}/>
                 </div>
                 <div style={{float: 'right', width: '70%', height:'100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'auto', flexDirection: 'column', position: 'relative', left: '-40px'}}>
-                    <label style={{fontFamily: 'Lato', fontSize: '25px', fontWeight:'bold', position: 'relative', left: '-100px'}}>8435 7999</label>
+                    <label style={{fontFamily: 'Lato', fontSize: '25px', fontWeight:'bold', position: 'relative', left: '-73px'}}>+506 8435 7999</label>
                     <label style={{fontFamily: 'Lato', fontSize: '25px', fontWeight:'bold', position: 'relative', left: '-20px'}}>jirodriguezx@gmail.com</label>
                 </div>
                 </div>
-                <img src={require('./Facebook Link.png')} alt={'Facebook Link'} style={{height:'12%',width:'4%', borderRadius: '10px', marginBottom:'10px'}}/>
-                <img src={require('./Location.PNG')} alt={'location'} style={{height:'55%',width:'35%'}}/>
+                <img src={require('./Facebook Link.png')} alt={'Facebook Link'} style={{height:'10%',width:'3%', borderRadius: '10px', marginBottom:'10px', position: 'relative', top:'-20px'}}/>
+                <img src={require('./Location.PNG')} alt={'location'} style={{height:'55%',width:'35%', position: 'relative', top: '-20px'}}/>
             
             </div>
         </Fragment>
